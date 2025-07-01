356306d80fbccd029ae3a5c942278d58486856df
tgncosist2 <tgncosist2@gmail.com>
2025-07-01 17:11:08 +0900

    asdasd

--- 88market/src/main/java/kr/co/sist/user/controller/LoginController.java
+++ 88market/src/main/java/kr/co/sist/user/controller/LoginController.java
@@ -3,6 +3,8 @@ package kr.co.sist.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.servlet.http.HttpSession;
@@ -42,5 +44,38 @@ public class LoginController {
 		
 		return "day0623/session_list";
 	} //modelGetValue
+	
+	@GetMapping("/login")
+	public String login() {
+		return "user/account/login";
+	}
+
+	@PostMapping("/login")
+	public String doLogin(@RequestParam("loginType") String loginType, HttpSession session) {
+		// 하드코딩된 유저 식별자 저장
+		switch (loginType) {
+			case "naver":
+				session.setAttribute("user", "user01");
+				break;
+			case "kakao":
+				session.setAttribute("user", "user02");
+				break;
+			case "email":
+				session.setAttribute("user", "user03");
+				break;
+		}
+		return "redirect:/"; // 로그인 후 메인페이지로 이동
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate(); // 세션 완전 초기화 (로그아웃)
+		return "redirect:/"; // 홈으로 이동
+	}
+
+	@GetMapping("/signup")
+	public String signup() {
+		return "user/account/signup";
+	}
 
 } //class
