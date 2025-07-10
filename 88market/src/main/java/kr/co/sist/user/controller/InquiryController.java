150a4ab2cdd6ad7a5236a17b08b7fda82ecdcae6
tgncosist2 <tgncosist2@gmail.com>
2025-07-10 23:14:04 +0900

    태일 11:13 0710 사용자 1:1문의 40%

--- 88market/src/main/java/kr/co/sist/user/controller/InquiryController.java
+++ 88market/src/main/java/kr/co/sist/user/controller/InquiryController.java
@@ -4,9 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.HttpServletRequest;
+import kr.co.sist.DTO.InquiryDTO;
 
 @Controller
 public class InquiryController {
@@ -14,8 +17,22 @@ public class InquiryController {
 	@GetMapping("/inquiry")
 	@ResponseBody
 	public ResponseEntity<String> inquiry(HttpServletRequest request) {
-		
-		return ResponseEntity.ok("야호");
+		return ResponseEntity.ok("로그인 체크 완료");
 	}// inquiry
+
+	@PostMapping("/inquiry/add")
+	@ResponseBody
+	public ResponseEntity<String> inquiryAdd(
+		MultipartFile[] files,
+		InquiryDTO inquiryDTO
+		) {
+
+			System.out.println(inquiryDTO);
+			for (MultipartFile file : files) {
+				System.out.println(file.getOriginalFilename());
+			}
+
+		return ResponseEntity.ok("문의 등록 완료");
+	}// inquiryAdd
 	
 }// class
