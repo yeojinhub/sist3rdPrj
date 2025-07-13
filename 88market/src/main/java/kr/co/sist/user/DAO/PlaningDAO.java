d1117ad055b6105078a1f4418155c7dee23f5b1d
rmk <dbaudrb99@naver.com>
2025-07-13 21:00:50 +0900

    0713 rmk2

--- 88market/src/main/java/kr/co/sist/user/DAO/PlaningDAO.java
+++ 88market/src/main/java/kr/co/sist/user/DAO/PlaningDAO.java
@@ -5,16 +5,17 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import kr.co.sist.DTO.CategoryWithProductWithFavoriteWithCompanyWithReviewWithImageDTO;
+import kr.co.sist.DTO.CategoryWithProductWithFavoriteWithCompanyWithImageDTO;
 import kr.co.sist.DTO.CompanyWithProductDTO;
 import kr.co.sist.DTO.ProductDTO;
+import kr.co.sist.DTO.ReviewDTO;
 
 @Mapper
 public interface PlaningDAO {
 
 	List<CompanyWithProductDTO> selectPlaningList();
 	List<ProductDTO> selectProductsByComNum(@Param("comNum") String comNum);
-	List<CategoryWithProductWithFavoriteWithCompanyWithReviewWithImageDTO> onePlaningList(@Param("prdNum") String prdNum);
+	List<CategoryWithProductWithFavoriteWithCompanyWithImageDTO> onePlaningList(@Param("prdNum") String prdNum);
 	List<ProductDTO> selectRandomProductsByComNum(@Param("comNum") String comNum,String productId);
 	int CompanyProductsAllCount(@Param("comNum") String comNum);
 	int CompanyProductsSellCount(@Param("comNum") String comNum);
@@ -22,5 +23,12 @@ public interface PlaningDAO {
 	List<ProductDTO> ProductsByCompanyAndSellType(@Param("comNum") String comNum,
             @Param("sellType") String sellType);
 	List<ProductDTO> AllProductsByCompany(@Param("comNum") String comNum);
+	List<ProductDTO> ProductsByCompanyAndSellTypeSort(@Param("comNum") String comNum,
+			@Param("sellType") String sellType,String sort);
+	List<ProductDTO> AllProductsByCompanySort(@Param("comNum") String comNum,String sort);
+	// 리뷰 목록 조회
+    List<ReviewDTO> selectReview(@Param("prdNum") String prdNum);
+    // 리뷰 개수 조회
+    int selectReviewCount(@Param("prdNum") String prdNum);
 	
 }
