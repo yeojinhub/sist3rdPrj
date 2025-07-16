b53c7ce394506b863a82449c33d8eda012b53e18
tgncosist2 <tgncosist2@gmail.com>
2025-07-16 21:23:14 +0900

    태일 0716 21:23 사용자 구매 50%

--- 88market/src/main/java/kr/co/sist/user/controller/BuyController.java
+++ 88market/src/main/java/kr/co/sist/user/controller/BuyController.java
@@ -1,14 +1,19 @@
 package kr.co.sist.user.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.sist.DTO.AddressDTO;
 import kr.co.sist.DTO.ImageDTO;
@@ -43,6 +48,13 @@ public class BuyController {
         model.addAttribute("addrList", addrList);
 
         return "user/product/buy";
+    }// buy
+
+    @PostMapping("/buy/payment")
+    @ResponseBody
+    public ResponseEntity<?> payment(@RequestParam("formData") Map<String, String> formData) {
+        System.out.println(formData);
+        return ResponseEntity.ok(Map.of("success", true));
     }
 	
 }
