92a12c5f2d2be5a6c9a53715053cb7d9131f2faa
yeojinhub <oks04028@gmail.com>
2025-07-21 06:14:53 +0900

    관리자-회원계정관리, 관리자계정관리 검색, 이미지 빼고 완료 yj

---
+++ 88market/src/main/java/kr/co/sist/admin/Service/AdminAccountAdminService.java
@@ -0,0 +1,89 @@
+package kr.co.sist.admin.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.sist.DTO.AdminDTO;
+import kr.co.sist.admin.DAO.AdminAccountAdminDAO;
+
+@Service
+public class AdminAccountAdminService {
+
+	@Autowired
+	private AdminAccountAdminDAO adminDAO;
+	
+	/**
+	 * 관리자 전체 조회
+	 * @return adminList 조회한 관리자 리스트
+	 */
+	public List<AdminDTO> searchAllAdmin(){
+		List<AdminDTO> adminList = new ArrayList<AdminDTO>();
+		
+		adminList = adminDAO.selectAdminList();
+		
+		System.out.println("AccountAdminService : List 값 "+adminList);
+		
+		return adminList;
+	} //searchAllAdmin
+	
+	/**
+	 * 관리자 단일 조회
+	 * @param admNum 조회할 관리자 번호
+	 * @return adminDTO 조회한 관리자 정보
+	 */
+	public AdminDTO searchOneAdmin(String admNum) {
+		AdminDTO adminDTO = adminDAO.selectOneAdmin(admNum);
+		
+		return adminDTO;
+	} //searchOneAdmin
+	
+	/**
+	 * 관리자 계정 등록
+	 * @param adminDTO 등록할 관리자 정보
+	 * @return flag 성공시 true, 실패 시 false 반환
+	 */
+	public boolean addAdmin(AdminDTO adminDTO) {
+		boolean flag = false;
+		
+		try {
+			boolean insertFlag = adminDAO.insertAdmin(adminDTO) == 1;
+			
+			if( insertFlag ) {
+				// 등록 성공
+				flag = true;
+			} //end if
+		} catch(Exception e) {
+			// 등록 실패
+			e.printStackTrace();
+		} //end try catch
+		
+		return flag;
+	} //addAdmin
+	
+	/**
+	 * 관리자 계정 수정
+	 * @param adminDTO 수정할 관리자 정보
+	 * @return flag 성공시 true, 실패 시 false 반환
+	 */
+	public boolean modifyAdmin(AdminDTO adminDTO) {
+		boolean flag = false;
+		
+		try {
+			boolean updateFlag = adminDAO.updateAdmin(adminDTO) == 1;
+			
+			if( updateFlag ) {
+				// 수정 성공
+				flag = true;
+			} //end if
+		} catch(Exception e) {
+			// 수정 실패
+			e.printStackTrace();
+		} //end try catch
+		
+		return flag;
+	} //modifyAdmin
+	
+} //class
---
+++ 88market/src/main/resources/static/js/admin/account/infoAdd.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const select = document.getElementById('select');
+	const input  = document.getElementById('email-domain');
+
+	select.addEventListener('change', function() {
+		const val = this.value;
+		
+		if (val === 'other' || val === '') {
+			input.value = '';
+			input.removeAttribute('readonly');
+			input.focus();
+		} else {
+			input.value = val;
+			input.setAttribute('readonly', 'readonly');
+		} //end if else
+	}); //addEventListener
+	
+	const btnAdd = document.getElementById("btnAdd");
+	
+	btnAdd.addEventListener("click", function () {
+		const name = document.getElementById("name").value;
+	    const nickname = document.getElementById("nickname").value;
+	    const tel = document.getElementById("tel").value;
+	    const email = document.getElementById("email-id").value + "@" + document.getElementById("email-domain").value;
+	    const pass = document.getElementById("pass").value;
+	    const zipcode = document.getElementById("zipcode").value;
+	    const address =
+	      document.getElementById("road-address").value +"/"+
+	      document.getElementById("detail-address").value + "/"+
+	      document.getElementById("extra-address").value;
+	    const regDate = document.getElementById("reg-date").value;
+		const banValue = document.querySelector('input[name="ban"]:checked').value;
+		const withdrawValue = document.querySelector('input[name="withdraw"]:checked').value;
+
+	    const jsonParam = {
+			name : name,
+			nickname : nickname,
+			tel : tel,
+			email : email,
+			pass : pass,
+			zipcode : zipcode,
+			address : address,
+			inputDate : regDate,
+			banType : banValue,
+			withdraw : withdrawValue
+		};
+		
+		fetch('/admin/account/userAddProcess', {
+			method: "POST",
+			headers: {
+				 "Content-Type": "application/json; charset=UTF-8",
+			 },
+			 body: JSON.stringify(jsonParam),
+		}).then((res) => {
+			if (!res.ok) throw new Error(`HTTP 에러 ${res.status}`);
+			return res.json();
+		}).then((data) => {
+			console.log(data);
+			if(data.result){
+				// 등록 성공
+				alert("회원 정보 등록이 완료되었습니다.");
+	    		// 이전 화면으로 이동
+	        	window.history.back();
+	    	} else {
+	    		// 등록 실패
+	    		alert(data.msg);
+	        } //end if else
+		}).catch((err) => {
+	        console.error("등록 오류:", err);
+		}); // 에러 처리
+	}); //btnAdd
+	
+	const btnBack = document.getElementById("btnBack");
+	btnBack.addEventListener("click", function () {
+		// 이전 화면으로 이동
+		window.history.back();
+	}); //btnBack
+	
+}); //DOMContentLoaded
